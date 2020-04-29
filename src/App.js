7a89ecff28a42dd8bfc98157d2a4f9b503b73a78
unknown <andrelopessfla@gmail.com>
2020-04-29 06:23:15 -0300

    criado o menu de navegação

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+    
+        
+
+        <a className="navbar-brand" href="#"><i className="fas fa-cog"></i>&nbsp; DEV</a>
+
+         <button className="navbar-toggler" type ="button" data-toggle="collapse"
+            data-target="#navbarMainToggle" aria-controls="navbarMainToggle" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button> 
+
+        <div className="collapse navbar-collapse" id="navbarMainToggle">
+
+            <div className="navbar-nav">
+              <a className="nav-item nav-link" href="#">Home</a>
+              <a className="nav-item nav-link" href="#">Channel</a>
+              <a className="nav-item nav-link" href="#">Login</a>
+            </div>
+
+        </div>
+        
+      
+    </nav>
+      
+
+    
+
     </div>
   );
 }

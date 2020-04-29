295540a2a07dfca354404a1a912555e9ede4bd2a
unknown <andrelopessfla@gmail.com>
2020-04-29 20:45:28 -0300

    criado o body da aplicação

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css'
 function App() {
   return (
     <div className="container-fluid p-0">
+
     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
     
         <button className="navbar-toggler" type ="button" data-toggle="collapse"
@@ -26,7 +27,7 @@ function App() {
             </div>
             <form className="form-inline" >
               <div className="input-group">
-                <div class="input-group-prepend">
+                <div className="input-group-prepend">
                     <span className="input-group-text">@</span>
                 </div>
                 <input type="text" className="form-control" placeholder="Username"></input>
@@ -39,13 +40,75 @@ function App() {
       
     </nav>
 
+      <section className="container p-2">
+
+      <ul className="list-unstyled">
+        <li className="media mt-2">
+          <img src="http://via.placeholder.com/150/771796" alt="" className="mr-3"/>
+          <div className="media-body">
+            <h5 className="mt-0 mb-1">Minha imagem</h5>
+            é simplesmente uma simulação de texto da indústria
+             tipográfica e de impressos, e vem sendo utilizado
+              desde o século XVI.
+          </div>
+        </li>
+        <li className="media mt-2">
+          <img src="http://via.placeholder.com/150/771796" alt="" className="mr-3"/>
+          <div className="media-body">
+            <h5 className="mt-0 mb-1">Minha imagem</h5>
+            é simplesmente uma simulação de texto da indústria
+             tipográfica e de impressos, e vem sendo utilizado
+              desde o século XVI.
+          </div>
+        </li>
+        <li className="media mt-2">
+          <img src="http://via.placeholder.com/150/771796" alt="" className="mr-3"/>
+          <div className="media-body">
+            <h5 className="mt-0 mb-1">Minha imagem</h5>
+            é simplesmente uma simulação de texto da indústria
+             tipográfica e de impressos, e vem sendo utilizado
+              desde o século XVI.
+          </div>
+        </li>
+        <li className="media mt-2">
+          <img src="http://via.placeholder.com/150/771796" alt="" className="mr-3"/>
+          <div className="media-body">
+            <h5 className="mt-0 mb-1">Minha imagem</h5>
+            é simplesmente uma simulação de texto da indústria
+             tipográfica e de impressos, e vem sendo utilizado
+              desde o século XVI.
+          </div>
+        </li>
+        <li className="media mt-2">
+          <img src="http://via.placeholder.com/150/771796" alt="" className="mr-3"/>
+          <div className="media-body">
+            <h5 className="mt-0 mb-1">Minha imagem</h5>
+            é simplesmente uma simulação de texto da indústria
+             tipográfica e de impressos, e vem sendo utilizado
+              desde o século XVI.
+          </div>
+        </li>
+        <li className="media mt-2">
+          <img src="http://via.placeholder.com/150/771796" alt="" className="mr-3"/>
+          <div className="media-body">
+            <h5 className="mt-0 mb-1">Minha imagem</h5>
+            é simplesmente uma simulação de texto da indústria
+             tipográfica e de impressos, e vem sendo utilizado
+              desde o século XVI.
+          </div>
+        </li>
+      </ul>
+
+      </section>
+
+
     <footer>
         <div className=" footer bg-dark text-light text-center">
         <div className="container col-6 col-lg-4 d-flex pt-3 justify-content-around ">
-              <i className="fab fa-facebook-square"></i>
-            <i class="fab fa-whatsapp-square"></i>
-            <i class="fab fa-linkedin"></i>
-            <i class="fab fa-instagram-square"></i>
+            <i className="fab fa-facebook-square"></i>
+            <i className="fab fa-whatsapp-square"></i>
+            <i className="fab fa-linkedin"></i>
+            <i className="fab fa-instagram-square"></i>
 
         </div>
         

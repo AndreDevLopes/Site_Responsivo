21abf2024c6d32ec18389c05c6e3a8b5d7cc8183
unknown <andrelopessfla@gmail.com>
2020-04-30 19:57:18 -0300

    criado carrosel

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ export default class App extends Component {
           <div className="collapse navbar-collapse" id="navbarMainToggle">
   
               <div className="navbar-nav ml-auto pr-3">
+              
                 <a className="nav-item nav-link" href="#">Home</a>
                 <a className="nav-item nav-link" href="#">Channel</a>
                 <a className="nav-item nav-link" href="#">Login</a>
@@ -52,43 +53,76 @@ export default class App extends Component {
         
       </nav>
   
+            <div id="carouselExampleIndicators" className="carousel slide m-2" data-ride="carousel">
+              <ol className="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              </ol>
+                 <div className="carousel-inner">
+
+                    {
+                      this.state.photos.map(photo =>{
+                        return(
+                          <div className="carousel-item active" key={photo.id}>
+                          <img className="d-block w-100 " src={photo.url} alt="First slide"/>
+
+                          </div>
+                        );
+                      })
+                    }
+                    
+                  <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                  </a>
+                  <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                  </a>
+                </div>
+            </div>
+
         <section className="container p-2">
-  
-        <ul className="list-unstyled">
-         {
-           this.state.photos.map(photo=>{
-             return(
-               <div key={photo.id}>
-                  <li className="media mt-2">
-                    <img src={photo.thumbnailUrl} alt="" className="mr-3"/>
-                    <div className="media-body">
-                      <h5 className="mt-0 mb-1">{photo.title}</h5>
-                     <p>{photo.id}-{photo.albumId}titulo: {photo.title} <br></br> link: {photo.url}</p>
-                    </div>
-                  </li>
-               </div>
-             )
-           })
-         }
-          
+
+
         
-        </ul>
+          
+                <ul className="list-unstyled">
+                {
+                  this.state.photos.map(photo=>{
+                    return(
+                      <div key={photo.id}>
+                          <li className="media mt-2">
+                            <img src={photo.thumbnailUrl} alt="" className="mr-3"/>
+                            <div className="media-body">
+                              <h5 className="mt-0 mb-1">{photo.title}</h5>
+                            <p>{photo.id}-{photo.albumId}titulo: {photo.title} <br></br> link: {photo.url}</p>
+                            </div>
+                          </li>
+                      </div>
+                    )
+                  })
+                }
+                  
+                
+                </ul>
   
         </section>
   
   
-      <footer>
-          <div className=" footer bg-dark text-light text-center">
-          <div className="container col-6 col-lg-4 d-flex pt-3 justify-content-around ">
-              <i className="fab fa-facebook-square"></i>
-              <i className="fab fa-whatsapp-square"></i>
-              <i className="fab fa-linkedin"></i>
-              <i className="fab fa-instagram-square"></i>
-  
-          </div>
+              <footer>
+                  <div className=" footer bg-dark text-light text-center">
+                  <div className="container col-6 col-lg-4 d-flex pt-3 justify-content-around ">
+                      <i className="fab fa-facebook-square"></i>
+                      <i className="fab fa-whatsapp-square"></i>
+                      <i className="fab fa-linkedin"></i>
+                      <i className="fab fa-instagram-square"></i>
           
-        </div>
-      </footer>
+                  </div>
+                  
+                </div>
+              </footer>
       
         
   
